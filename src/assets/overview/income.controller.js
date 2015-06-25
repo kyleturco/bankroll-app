da4071d51e2007e8b80c38eff8ef616fd464ca65
Kyle Turco <kyleturco@gmail.com>
2015-06-25 11:43:52 -0500

    progress with firebase saving

--- src/assets/overview/income.controller.js
+++ src/assets/overview/income.controller.js
@@ -1,27 +1,26 @@
 angular
   .module('bankRoll')
 
-  .controller('IncomeController', function ($routeParams, $location, Income) {
+  .controller('IncomeController', function ($scope, $routeParams, $location, incomeFactory) {
     var vm = this;
-    vm.id = $routeParams.id;
+    // vm.id = $routeParams.id;
 
-
-    Income.getOne(vm.id, function (data) {
-      vm.income = data;
-    });
+    // Income.getOne(vm.id, function (data) {
+    //   vm.income = data;
+    // });
 
     vm.destroy = function (id) {
-      Income.destroy(vm.id, function () {
+      incomeFactory.destroy(vm.id, function () {
         $location.path('/overview');
       });
     };
 
     vm.saveIncome = function () {
-      debugger;
-      Income.create(vm.id, function () {
+      console.log(vm.main);
+      incomeFactory.create(vm.main.income, function (data) {
         console.log("is this shit working?");
       });
     };
 
     // vm.onModalLoad = function () {};
-  });
+});
--- src/assets/overview/income.factory.js
+++ src/assets/overview/income.factory.js
@@ -1,36 +1,41 @@
 angular
   .module('bankRoll')
 
-  .factory('Income', function ($http, API_URL) {
-    return {
-      getOne(id, cb) {
-        $http
-          .get(`${API_URL}profiles/${authData.uid}.json`)
-          .success(cb);
-      },
+  .factory('incomeFactory', function ($rootScope, $http, API_URL) {
 
-      getAll(cb) {
-        $http
-          .get(`${API_URL}profiles/${authData.uid}.json`)
-          .success(cb);
-      },
+    function incomeUrl(id) {
+      return API_URL + '/profiles/' + $rootScope.auth.uid + '/income.json?auth=' + $rootScope.auth.token;
+    }
+
+    return {
+      // getOne(id, cb) {
+      //   $http
+      //     .get(`${API_URL}profiles/${authData.uid}.json`)
+      //     .success(cb);
+      // },
+
+      // getAll(cb) {
+      //   $http
+      //     .get(`${API_URL}profiles/${authData.uid}.json`)
+      //     .success(cb);
+      // },
 
       create(data, cb) {
         $http
-          .post(`${API_URL}profiles/${fb.getAuth().uid}/income`, data)
+          .post(incomeUrl(), data)
           .success(cb);
       },
 
-      update(id, data, cb) {
-        $http
-          .put(`${API_URL}profiles/${authData.uid}.json`, data)
-          .success(cb);
-      },
+      // update(id, data, cb) {
+      //   $http
+      //     .put(`${API_URL}profiles/${authData.uid}.json`, data)
+      //     .success(cb);
+      // },
 
-      destroy(id, cb) {
-        $http
-          .delete(`${API_URL}profiles/${authData.uid}.json`)
-          .success(cb);
-      }
+      // destroy(id, cb) {
+      //   $http
+      //     .delete(`${API_URL}profiles/${authData.uid}.json`)
+      //     .success(cb);
+      // }
     }
   });
